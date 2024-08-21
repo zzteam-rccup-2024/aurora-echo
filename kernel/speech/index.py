6cc64ab6ca19eca6c5c0c4b3a35a552fa3445b70
Ethan Goh <7086cmd@gmail.com>
2024-08-21 14:56:56 +0800

    chore: add check availability for whisper.

--- kernel/speech/index.py
+++ kernel/speech/index.py
@@ -1,5 +1,6 @@
 from kernel.speech.recognize import Wav2Vec
 from kernel.speech.whisper import Whisper
+from kernel.availability import check_availability
 
 
 class SpeechToTextModel:
@@ -8,7 +9,7 @@ class SpeechToTextModel:
 
     def switch(self, model: str = 'whisper'):
         if model == 'whisper':
-            self.model = Whisper()
+            self.model = Whisper('openai-whisper' if check_availability() else 'openai-proxied-whisper')
         elif model == 'wav2vec':
             self.model = Wav2Vec()
         else:
